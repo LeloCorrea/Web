29ba266dab3cd32b63a785f701e4a5c7e7231ad0
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-26 00:24:54 -0300

    Teste

--- teste.js
+++ teste.js
@@ -3,3 +3,12 @@ var sobrenome = "Cunha";
 var idade = 30;
 var resultado = (nome+" "+sobrenome+" "+"terá"+" "+idade+" "+"anos");
 console.log(resultado)
+---
+var numeroA = 30;
+var numeroB = 45;
+var numeroC = numeroA;
+numeroA = numeroB;
+numeroB = numeroC;
+---
+var gostoDeSorvete = true;
+console.log(gostoDeSorvete)
